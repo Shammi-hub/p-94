20099a2bacc18f4cc356fa4e24b68dc5d3bd657a
Shammi-hub <80632841+Shammi-hub@users.noreply.github.com>
2021-07-21 19:58:58 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCaWr1_jFcJDWmYBIMLNC_RrUdAUR2noIg",
+  authDomain: "p-94-9225c.firebaseapp.com",
+  projectId: "p-94-9225c",
+  storageBucket: "p-94-9225c.appspot.com",
+  messagingSenderId: "4873801878",
+  appId: "1:4873801878:web:f8a56ce2413729d425b2b7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
